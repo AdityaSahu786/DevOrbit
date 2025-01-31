6053204ab1ce2a7b09602c35f695101a67cb5db3
AdityaSahu786 <sahuadi786183@gmail.com>
2025-01-31 22:45:22 +0530

    Setup Airdrop

---
+++ Week_5.1/react-week5-web3/src/Airdrop.jsx
@@ -0,0 +1,5 @@
+export function Airdrop() {
+    return <div>
+        hi airdrop
+    </div>
+} 
--- Week_5.1/react-week5-web3/src/App.jsx
+++ Week_5.1/react-week5-web3/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
@@ -8,25 +8,30 @@ import {
     WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
+import { Airdrop } from './Airdrop';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // ✅ Fix: Define wallets array
+  const wallets = useMemo(() => [new UnsafeBurnerWalletAdapter()], []);
 
   return (
-  
-      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
-            <WalletProvider wallets={[]} autoConnect>
-                <WalletModalProvider>
-                    <div>
-                      hi there! <b>buddy</b>
-                    </div>
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-  )
+    <ConnectionProvider endpoint={clusterApiUrl('devnet')}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <div>
+            hi there! <b>buddy</b>
+          </div>
+          <WalletMultiButton /> {/* ✅ Fix: Add a connect wallet button */}
+          <Airdrop />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
-export default App
+export default App;
