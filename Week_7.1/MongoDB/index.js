9c49b63fef6f7e7c7d425aabfd777a484ccc9097
AdityaSahu786 <sahuadi786183@gmail.com>
2025-02-10 19:16:24 +0530

    Defined Schema

---
+++ Week_7.1/MongoDB/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { UserModel, TodoModel } = require("./db");
+const app = express();
+
+app.post("/signup", function(req, res) {
+    
+})
+
+app.post("/signin", function(req, res) {
+    
+})
+
+app.post("/todo", function(req, res) {
+    
+})
+
+app.get("/todo", function(req, res) {
+    
+})
