d89cdba989f506f94410c2319637d58b6ab5d629
AdityaSahu786 <sahuadi786183@gmail.com>
2025-02-10 23:08:32 +0530

    Setting up endpoints

--- Week_7.1/MongoDB/index.js
+++ Week_7.1/MongoDB/index.js
@@ -1,14 +1,46 @@
 const express = require('express');
 const { UserModel, TodoModel } = require("./db");
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "ramDEVbAbaKIjaiHO";
 const app = express();
 
-app.post("/signup", function(req, res) {
+app.post("/signup", async function(req, res) {
     
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+     await UserModel.insert({
+        email: email,
+        password: password,
+        name: name
+     })
+       res.json({
+         message: "You are logged in"
+       })
 })
 
 app.post("/signin", function(req, res) {
     
-})
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = UserModel.findOne({
+        email: email,
+        password: password
+    })
+
+    if (user) {
+        const token = "";
+        res.json ({
+
+        });
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
+});
 
 app.post("/todo", function(req, res) {
     
