27a05a4c7fd92735b5146822b70f3d2b5af0d866
AdityaSahu786 <sahuadi786183@gmail.com>
2025-02-12 19:40:00 +0530

    token launchpad

---
+++ Week_8.3/week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { TokenLaunchpad } from './components/TokenLaunchpad'
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+function App() {
+  return (
+    <TokenLaunchpad></TokenLaunchpad>
+  )
+}
+
+export default App
