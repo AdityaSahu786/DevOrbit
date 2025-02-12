8ab56f76b11b4854b6d6e42a859e4175a69d419c
AdityaSahu786 <sahuadi786183@gmail.com>
2025-02-12 10:03:42 +0530

    Solana Faucet

--- Week_8.2/week-5-dapp/src/App.jsx
+++ Week_8.2/week-5-dapp/src/App.jsx
@@ -1,6 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
+
 import './App.css'
 
 function App() {
@@ -8,7 +17,15 @@ function App() {
 
   return (
     <>
-      hi there Buddy!
+     <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+     <WalletProvider wallets={[]} autoConnect>
+     <WalletModalProvider>
+        <div>
+          <WalletMultiButton/>
+        </div>
+     </WalletModalProvider>
+     </WalletProvider>
+     </ConnectionProvider>
     </>
   )
 }
