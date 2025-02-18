8ef1cff1bff6b685d19b1200176d1c619ae8f19f
AdityaSahu786 <sahuadi786183@gmail.com>
2025-02-18 23:32:24 +0530

    feat: counter is live.  re-renders? handled.

--- Week_9.1_Foundations/react-fundamentals/src/App.jsx
+++ Week_9.1_Foundations/react-fundamentals/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>hello from neuralnets!</h1>
-    </>
-  )
-}
+   function onClickHandler() {
+    setCount(count + 1);
+   }
 
-export default App
+   return (
+    <div>
+      <button id="btn" onClick={onClickHandler}>
+        Counter  {count}
+      </button>
+    </div>
+   )
+};
