ddca735e7f9218fbc35fe909dc827d89e457c661
AdityaSahu786 <sahuadi786183@gmail.com>
2025-02-19 17:07:52 +0530

    Feat: Initial commit. Vite + React Setup

---
+++ Week_9.2/week-9-offline/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>Hi there from NeuralNets!</div>
+  )
+}
+
+
+
+export default App
